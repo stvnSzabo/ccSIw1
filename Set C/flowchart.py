f03d7ac1d7bb8370380ad2fa3b5c3460640bbfd0
stvnSzabo <55262494+stvnSzabo@users.noreply.github.com>
2019-09-13 02:20:08 +0200

    Create flowchart.py

---
+++ Set C/flowchart.py
@@ -0,0 +1,26 @@
+numbers = []
+leng = int(input("How many numbers:"))
+
+for i in range(leng):
+    numbers.append(int(input("Enter number: ")))
+n = len(numbers)
+
+m = 1
+while m < n:
+    j = 0
+    while j <= n - 2:
+        if numbers[j] > numbers [j+1]:
+            temp = numbers[j+1]
+            numbers[j+1] = numbers[j]
+            numbers[j] = temp
+        else:
+            j += 1
+    else:
+        m += 1
+
+
+
+
+
+
+print(numbers)
